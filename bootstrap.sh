a2556d71145481023071952d9d266e1484265628
Owajigbanam Ogbuluijah <igbanam@andela.com>
2018-09-24 18:41:56 +0100

    Begin on Bootstrap
    My laptop got stolen again! It's almost like the gods are trying to tell
    me something and I haven't been hearing since. Not anymore. I have
    heard. It currently takes me almost a day to get up to speed with all my
    work tools. I have to reduce this to an hour.
    
    This change
    
    - introduces a bootstrap shell file as an entry point for installation
    - begins curating my "toolstoy" (sorry, I couldn't help myself 🤪)
    - adds a new GPG from the new computer

---
+++ bootstrap.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install Brew toys
+if [ -x which brew ]; then
+  cat toolstoy | xargs brew install
+fi
+
+if [ ! -f "~/.bashrc" ]; then
+  touch ~/.bashrc
+fi
+
+cat << BASH_GIT_PROMPT_CONFIG >> ~/.bashrc
+  if [ -f "/usr/local/opt/bash-git-prompt/share/gitprompt.sh" ]; then
+    __GIT_PROMPT_DIR="/usr/local/opt/bash-git-prompt/share"
+    source "/usr/local/opt/bash-git-prompt/share/gitprompt.sh"
+  fi
+BASH_GIT_PROMPT_CONFIG
+
+source ~/.bashrc
+
+# Install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Projects Directory
+if [ ! -d "~/projects" ]; then
+  mkdir ~/.projects
+fi
+
+# Clone the "Configuration of the Gods"
+echo Clone the "Configuration of the Gods"
+
+# Copy the VIMRC into place
+echo "Copy the VIMRC into place"
