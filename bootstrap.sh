0a369a17feba43c9b8d8f0663ccc028da0288a5d
Owajigbanam Ogbuluijah <igbanam@andela.com>
2019-10-11 06:00:57 +0100

    updated bootstrap: used helper files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 echo "Setup critical environment variables..."
 export COTG_PATH
 COTG_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "Bring in the helper dwarfs..."
+PATH="$COTG_PATH/bin:$PATH"
+
 echo "Checking for updates..."
-if is-executable git -a -d "$DOTFILES_DIR/.git"; then
-  git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master;
+if is-executable git -a -d "$COTG_PATH/.git"; then
+  git --work-tree="$COTG_PATH" --git-dir="$COTG_PATH/.git" pull origin master;
 fi
 
 echo "Installing Homebrew..."
